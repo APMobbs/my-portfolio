8c07d5af5cc1af4a1a20fae1c0dbe623113132db
alex.prince-mobbs <alex.prince-mobbs@outlook.com>
2019-08-28 15:52:10 +0100

    Update 28/08 15:52

---
+++ js/objects.js
@@ -0,0 +1,90 @@
+// let offer = "name"
+// let time = 1200
+
+// const cafe = {
+//     name: "whitesheep",
+//     seatingCapcity: 100,
+//     hasSpecialOffers: true,
+//     drinks: [
+//         "coffee 1",
+//         "coffee 2",
+//         "coffee 3",
+//         "coffee 4",
+//         "coffee 5"
+//     ],
+//     breakastOffer: "Free Croissant with coffee",
+//     lunchOffer: "Free drink with lunch",
+//     none: "no offer",
+
+//     openCafe(){
+//         if (this.hasSpecialOffers){
+//         return "time for the special"
+//         }
+//     },
+//     closeCafe(){
+//         return "we are closed, come back tomorrow"
+//     }
+// }
+
+// console.log(cafe.openCafe())
+
+// if (time < 1000){
+//     offer = cafe.breakastOffer
+// } else if (time < 1500){
+//     offer = cafe.lunchOffer
+// } 
+
+// console.log(offer)
+
+const person = {
+    name: "Yigrus",
+    age: 2340,
+    favSong: [
+        "Rewirte by Darling Theives",
+        "Unravel by Toru Kitajima",
+        "La Vie En Rose by Édith Piaf",
+        "Holding out for a Hero - Bonnie Tyler"
+    ],
+
+    sayHi(){
+        return `hello, my name is ${this.name}`
+    }
+}
+
+console.log(person.sayHi())
+
+
+
+
+
+// let day = "sunday"
+
+// const Alarm = {
+//     weekdayAlarm: "alarm set 7am",
+//     weekendAlarm: "no alarm"
+// }
+
+// let alarm = ""
+
+// if(day == "saturday" || day == "sunday"){
+//     alarm = Alarm.weekendAlarm
+// } else (
+//     alarm = Alarm.weekdayAlarm
+// )
+
+// console.log(`it is ${day} so that means ${alarm}`)
+
+// switch(day){
+//     case "monday":
+//         console.log("the")
+//         break;
+//     case "tuesday":
+//             console.log("the")
+//             break;
+//     case "wednsday": 
+//     console.log("the")
+//     break;
+// default:
+//     console.log("something is wrong")
+// }
+
